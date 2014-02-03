79599169c628eafffe9ced0363247f54f56cd4b9
Alexis Bellido <alexis@ventanazul.com>
2014-02-03 10:43:42 -0500

    Initial package created to run a simple class.

---
+++ src/defaultPackage/JavaBasicProgram.java
@@ -0,0 +1,10 @@
+package defaultPackage;
+
+public class JavaBasicProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("The code will go here for Java Eclipse project.");
+    }
+}
+
---
+++ src/defaultPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author alexis
+ *
+ */
+package defaultPackage;
