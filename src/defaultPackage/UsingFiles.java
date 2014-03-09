645f25baa3371ee6949c6482d73b40352d84f04e
Alexis Bellido <alexis@ventanazul.com>
2014-03-09 16:27:50 -0400

    write to file

--- src/defaultPackage/UsingFiles.java
+++ src/defaultPackage/UsingFiles.java
@@ -7,10 +7,15 @@ public class UsingFiles {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		String fileName = "/home/alexis/Downloads/myfile.txt";
 		System.out.println("Read a file line by line");
+		PrintWriter out = new PrintWriter(fileName);
+		out.println("A new line");
+		out.println("A new line 2");
+		out.close();
 //		File f = new File("/home/alexis/Downloads/myfile.txt");
 //		Scanner in = new Scanner(f);
-		Scanner in = new Scanner(new File("/home/alexis/Downloads/myfile.txt"));
+		Scanner in = new Scanner(new File(fileName));
 		while (in.hasNextLine())
 		{
 			String line = in.nextLine();
