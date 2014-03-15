d87a45feb42989f23c1e026a15e31db35631b4dd
Alexis Bellido <alexis@ventanazul.com>
2014-03-15 18:31:08 -0400

    Separate blocks with Alt + Shift + Up shortcut

--- src/defaultPackage/TryArrays.java
+++ src/defaultPackage/TryArrays.java
@@ -6,11 +6,13 @@ public class TryArrays {
 		int[] numbers = new int[5];
 		double[] squares = new double[5];
 		int[] smallPrimes = {2, 3, 5, 7};
+		
 		for (int i = 0; i < numbers.length; i++)
 		{
 			numbers[i] = i + 1;
 			squares[i] = Math.pow(i + 1, 2);
 		}
+		
 		for (int i = 0; i < 5; i++)
 		{
 			System.out.println("number: " + numbers[i] + " square: " + squares[i]);
