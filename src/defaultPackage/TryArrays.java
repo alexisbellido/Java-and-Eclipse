380a6694abccb5deb6449306fd12b18bf7db0fbc
Alexis Bellido <alexis@ventanazul.com>
2014-03-15 18:16:35 -0400

    Create array with shorthand notation

--- src/defaultPackage/TryArrays.java
+++ src/defaultPackage/TryArrays.java
@@ -5,6 +5,7 @@ public class TryArrays {
 	public static void main(String[] args) {
 		int[] numbers = new int[5];
 		double[] squares = new double[5];
+		int[] smallPrimes = {2, 3, 5, 7};
 		for (int i = 0; i < numbers.length; i++)
 		{
 			numbers[i] = i + 1;
@@ -13,6 +14,16 @@ public class TryArrays {
 		for (int i = 0; i < 5; i++)
 		{
 			System.out.println("number: " + numbers[i] + " square: " + squares[i]);
+		}
+		
+		for (int number: numbers)
+		{
+			System.out.println("number using foreach: " + number);
+		}
+		
+		for (int prime: smallPrimes)
+		{
+			System.out.println("small prime using foreach: " + prime);
 		}
 	}
 
