ee6e87c8aa33a4f7a7b62587b4522749cb39b975
Alexis Bellido <alexis@ventanazul.com>
2014-04-19 17:01:10 -0400

    stocks package

---
+++ src/com/zinibu/stocks/Stock.java
@@ -0,0 +1,12 @@
+package com.zinibu.stocks;
+
+public class Stock {
+	public static void main(String[] args) {
+		sayHi();
+	}
+	
+	private static void sayHi() {
+		System.out.println("Hi!");
+	}
+
+}
