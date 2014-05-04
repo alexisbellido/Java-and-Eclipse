5a9937e8cd74e7c441f9afebfdc6fcba4c237a32
Alexis Bellido <alexis@ventanazul.com>
2014-05-03 23:06:48 -0400

    create package for basic stuff

--- src/com/zinibu/basic/UsingFiles.java
+++ src/com/zinibu/basic/UsingFiles.java
@@ -1,4 +1,4 @@
-package defaultPackage;
+package com.zinibu.basic;
 
 import java.util.*;
 import java.io.*;
@@ -7,24 +7,23 @@ public class UsingFiles {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		String fileName = "/home/alexis/Downloads/myfile.txt";
-		int age = 10;
-		System.out.println("Read a file line by line");
-		PrintWriter out = new PrintWriter(fileName);
-		out.println("A new line");
-		out.println("A new line 2");
-		out.close();
+//		String fileName = "/home/alexis/Downloads/myfile.txt";
+		String fileName = "/Users/alexis/test.txt";
+		
+//		System.out.println("Read a file line by line");
+//		PrintWriter out = new PrintWriter(fileName);
+//		out.println("A new line");
+//		out.println("A new line 2");
+//		out.close();
 //		File f = new File("/home/alexis/Downloads/myfile.txt");
 //		Scanner in = new Scanner(f);
+//		File f = new File("/Users/alexis/test.txt");
+		
 		Scanner in = new Scanner(new File(fileName));
 		while (in.hasNextLine())
 		{
 			String line = in.nextLine();
 			System.out.println(line);
-		}
-		if (age == 10 )
-		{
-			System.out.printf("Hey there, you are %d", age);
 		}
 
 	}
