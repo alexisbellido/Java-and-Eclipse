7734e0cf7da806b18937aed42ef643020aeba56b
Alexis Bellido <alexis@ventanazul.com>
2014-05-06 23:10:04 -0400

    extra settings for JFrame

--- src/com/zinibu/employee/SwingFrame.java
+++ src/com/zinibu/employee/SwingFrame.java
@@ -1,14 +1,26 @@
 package com.zinibu.employee;
 
 import javax.swing.JFrame;
-import java.awt.Color;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class SwingFrame extends JFrame {
 	public static final int DEFAULT_WIDTH = 300;
 	public static final int DEFAULT_HEIGHT = 200;
 	
 	public SwingFrame() {
-		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		setSize(screenWidth / 3, screenHeight / 2);
+//		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		setTitle("The SwingTest");
+		setLocationByPlatform(true);
+
+		
 	}
 
 }
