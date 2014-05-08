59ddbf49dfb6c1fef143697643f33c39442c1e50
Alexis Bellido <alexis@ventanazul.com>
2014-05-07 20:48:57 -0400

    add JPanel and JButton

--- src/com/zinibu/employee/SwingFrame.java
+++ src/com/zinibu/employee/SwingFrame.java
@@ -4,23 +4,32 @@ import javax.swing.JFrame;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import javax.swing.SpringLayout;
+import javax.swing.JPanel;
 
 public class SwingFrame extends JFrame {
-	public static final int DEFAULT_WIDTH = 300;
-	public static final int DEFAULT_HEIGHT = 200;
+	public static final int DEFAULT_WIDTH = 500;
+	public static final int DEFAULT_HEIGHT = 400;
+	private SwingPanel currentPanel;
 	
 	public SwingFrame() {
-		Toolkit kit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = kit.getScreenSize();
-		int screenHeight = screenSize.height;
-		int screenWidth = screenSize.width;
-		setSize(screenWidth / 3, screenHeight / 2);
-//		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		currentPanel = new SwingPanel();
+		setupFrame();
 		
+//		Toolkit kit = Toolkit.getDefaultToolkit();
+//		Dimension screenSize = kit.getScreenSize();
+//		int screenHeight = screenSize.height;
+//		int screenWidth = screenSize.width;
+//		setSize(screenWidth / 3, screenHeight / 2);
+		
+//		SpringLayout springLayout = new SpringLayout();
+//		getContentPane().setLayout(springLayout);
+	}
+	
+	private void setupFrame() {
+		setContentPane(currentPanel);
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setTitle("The SwingTest");
 		setLocationByPlatform(true);
-
-		
 	}
-
 }
--- src/com/zinibu/employee/SwingPanel.java
+++ src/com/zinibu/employee/SwingPanel.java
@@ -1,12 +1,32 @@
 package com.zinibu.employee;
 
-import javax.swing.JPanel;
-import javax.swing.UIManager;
 import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+
 public class SwingPanel extends JPanel {
+	
+	private JButton testButton;
+	private JTextArea testTextArea;
+	private SpringLayout currentLayout;
+	
 	public SwingPanel() {
+		testButton = new JButton("Click here");
+		testTextArea = new JTextArea();
+		currentLayout = new SpringLayout();
+		currentLayout.putConstraint(SpringLayout.NORTH, testButton, 137, SpringLayout.NORTH, this);
+		currentLayout.putConstraint(SpringLayout.WEST, testButton, 173, SpringLayout.WEST, this);
+		setupPanel();
+	}
+	
+	private void setupPanel() {
 		setBackground(new Color(102, 0, 204));
+		setLayout(currentLayout);
+		add(testButton);
+		add(testTextArea);
 	}
 
 }
