6921f8395400459d4a8790efb8f2d6a64a8ef5c6
Alexis Bellido <alexis@ventanazul.com>
2014-05-07 21:10:06 -0400

    draw a Rectangle2D on a JPanel

--- src/com/zinibu/employee/SwingPanel.java
+++ src/com/zinibu/employee/SwingPanel.java
@@ -1,32 +1,50 @@
 package com.zinibu.employee;
 
-import java.awt.Color;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
 
 public class SwingPanel extends JPanel {
 	
 	private JButton testButton;
 	private JTextArea testTextArea;
 	private SpringLayout currentLayout;
+	public static final int MESSAGE_X = 75;
+	public static final int MESSAGE_Y = 100;
 	
 	public SwingPanel() {
 		testButton = new JButton("Click here");
 		testTextArea = new JTextArea();
-		currentLayout = new SpringLayout();
-		currentLayout.putConstraint(SpringLayout.NORTH, testButton, 137, SpringLayout.NORTH, this);
-		currentLayout.putConstraint(SpringLayout.WEST, testButton, 173, SpringLayout.WEST, this);
 		setupPanel();
 	}
 	
 	private void setupPanel() {
-		setBackground(new Color(102, 0, 204));
+		testTextArea.setRows(2);
+		testTextArea.setColumns(1);
+		currentLayout = new SpringLayout();
+		currentLayout.putConstraint(SpringLayout.NORTH, testTextArea, 34, SpringLayout.SOUTH, testButton);
+		currentLayout.putConstraint(SpringLayout.WEST, testTextArea, 0, SpringLayout.WEST, testButton);
+		currentLayout.putConstraint(SpringLayout.EAST, testTextArea, 0, SpringLayout.EAST, testButton);
+		currentLayout.putConstraint(SpringLayout.NORTH, testButton, 137, SpringLayout.NORTH, this);
+		currentLayout.putConstraint(SpringLayout.WEST, testButton, 173, SpringLayout.WEST, this);
+		setBackground(new Color(204, 255, 204));
 		setLayout(currentLayout);
 		add(testButton);
 		add(testTextArea);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("Hi, I am a string", MESSAGE_X, MESSAGE_Y);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		double leftX = 100;
+		double topY = 100;
+		double width = 200;
+		double height = 150;
+		
+		Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
+		g2.draw(rect);
 	}
 
 }
